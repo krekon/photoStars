2802dcb0c376025034f78e928d62cbd2770eaf29
krekon <palianthropos@hotmail.com>
2016-06-24 17:10:39 +0300

    Διορθωση του πλαισίου της εικόνας όταν η οθόνη μικραίνει πολύ. Μεταττροπή της υλοποίησης από div σε canvas

--- public_html/js/editImage.js
+++ public_html/js/editImage.js
@@ -3,107 +3,63 @@
                         width: 0,
                         height: 0,
                         relIconWidth: 0,
-                        relIconHeight: 0
+                        relIconHeight: 0,
+                        usersRealImgWidth:0,
+                        usersRealImgHeight: 0,
+                        usersRelImgWidth: 0,
+                        usersRelImgHeight: 0,
+                        finalIconWidth: 0,
+                        finalIconHeight:  0,
+                        finalpositionWidth: 0,
+                        finalpositionHeight: 0
                     };
+                    
+                    
 
                     function SaveIT()
                     {
-                        finalposition.width;
-                        finalposition.height;
-                        finalposition.relIconWidth;
-                        finalposition.relIconHeight;
+                        
+                        finalposition.finalIconWidth = finalposition.relIconWidth*finalposition.usersRealImgWidth;
+                        finalposition.finalIconHeight = finalposition.relIconHeight*finalposition.usersRealImgHeight;
+                        finalposition.finalpositionWidth = finalposition.width*finalposition.usersRealImgWidth;
+                        finalposition.finalpositionHeight = finalposition.height*finalposition.usersRealImgHeight;
+                        
+                        return finalposition;
                         
                     }
 
-                    function hideBtnSP() {
-                        $("#divSP").fadeOut("slow");
-                    }
-                    function showBtnSP() {
-                        $("#divSP").fadeIn("slow");
-
-                    }
-
-                    function MoveBtn() {
-                        var btnoffset = $("#btnSavePosition").offset();
-                        var xPosbtn = btnoffset.left;
-                        var yPosbtn = btnoffset.top;
-
-                        var IWidth = document.getElementById('dragThis').clientWidth;
-                        var IHeight = document.getElementById('dragThis').clientHeight;
-
-                        var BWidth = document.getElementById('btnSavePosition').clientWidth;
-                        var BHeight = document.getElementById('btnSavePosition').clientHeight;
-
-                        if (Math.abs(finalposition.width - xPosbtn) < (IWidth + BWidth / 2) && Math.abs(finalposition.height - yPosbtn) < (IHeight / 2 + BHeight / 2))
-                        {
-                            if (document.getElementById("divSP").style.left === "-35%")
-                            {
-                                document.getElementById("divSP").style.left = "35%";
-                            }
-                            else
-                            {
-                                document.getElementById("divSP").style.left = "-35%";
-                            }
-
-                        }
-
-                    }
-                    
-                    
                    
-                    function divTouchImg()
-                    {
-                        var image_url = $('#mainImage').css('background-image'),
-                                image;
-
-
-                        image_url = image_url.match(/^url\("?(.+?)"?\)$/);
-
-                        if (image_url[1]) {
-                            image_url = image_url[1];
-                            image = new Image();
-
-
-                            $(image).load(function () {
-                                
-                                 
-                                $('#mainImage').css('width', image.width);
-                                $('#mainImage').css('height', image.height);
-                                
-                                var middleW = document.getElementById("mainImage").clientWidth;
-                                var newH = image.height / image.width * middleW;
-
-                                var middleH = document.getElementById("mainImage").clientHeight;
-                                var newW = image.width / image.height * middleH;
-
-                                $("#mainImage").css("height", newH.toFixed(0));
-                                $("#mainImage").css("width", newW.toFixed(0));
-                    
-                            });
-
-                            image.src = image_url;
-                        }
-
-                    }
-
                     function makeimage(image1url, image2url) {
 
-                        $("#dragThis").attr("src", image1url);
-                        document.getElementById("mainImage").style.backgroundImage = "url(" + image2url + ")";
+                        var image = new Image();
+                       
+                        image.onload = function () {
+                            
+                            RealImgDimensions(image.width,image.height);
+                            
+                            var canvas = document.getElementById("cnvs");
+                            canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+                            canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+                            var context = canvas.getContext('2d');
+                            context.drawImage(this, 0, 0);
+                            
+                            dataUrl = canvas.toDataURL();
+                            document.getElementById("cnvs").style.background='url('+dataUrl+')';
+                            
+                        };
 
-                        divTouchImg();
+                        image.src = image2url;
                         
-                        $('#dragThis').draggable(
+                        $("#dragThis").attr("src", image1url);
+                            $('#dragThis').draggable(
                                 {
                                     drag: function ()
                                     {
                                         var $this = $(this);
                                         var thisPos = $this.position();
-                                        var parentPos = $this.parent().position();
-
-                                        var x = (thisPos.left - parentPos.left)/document.getElementById("mainImage").clientWidth;
-                                        var y = (thisPos.top - parentPos.top)/document.getElementById("mainImage").clientHeight;
-
+                                        
+                                        var x = thisPos.left/document.getElementById("cnvs").clientWidth;
+                                        var y = thisPos.top/document.getElementById("cnvs").clientHeight;
 
                                         finalposition.width = x;
                                         finalposition.height = y;
@@ -111,48 +67,45 @@
                                     containment: "parent"
 
                                 });
-                                
-    
-                        $(window).resize(function () {
-                            $('.mymainimage').css({height: "100%",
-                                width: "100%"});
-
-                        });
-
+                        
                     }
                     
                     function RelativeIconDimensions()
                     {
-                        finalposition.relIconWidth = document.getElementById("dragThis").clientWidth/document.getElementById("mainImage").clientWidth;
-                        finalposition.relIconHeight = document.getElementById("dragThis").clientHeight/document.getElementById("mainImage").clientHeight;
+                        finalposition.relIconWidth = (document.getElementById("dragThis").clientWidth/document.getElementById("cnvs").clientWidth).toFixed(5);
+                        finalposition.relIconHeight = (document.getElementById("dragThis").clientHeight/document.getElementById("cnvs").clientHeight).toFixed(5);
+                    }
+                    
+                    function RealImgDimensions(width,height)
+                    {
+                        finalposition.usersRealImgWidth = width;
+                        finalposition.usersRealImgHeight = height;
+                    }
+                    
+                    function RelativeImgDimensions()
+                    {
+                        finalposition.usersRelImgWidth = document.getElementById("cnvs").clientWidth;
+                        finalposition.usersRelImgHeight = document.getElementById("cnvs").clientHeight;
                     }
 
                     function startingPosition()
                     {
                         var $this = $("#dragThis");
                         var thisPos = $this.position();
-                        var parentPos = $this.parent().position();
 
-                        var x = (thisPos.left - parentPos.left)/document.getElementById("mainImage").clientWidth;
-                        var y = (thisPos.top - parentPos.top)/document.getElementById("mainImage").clientHeight;
-
+                        var x = (thisPos.left/document.getElementById("cnvs").clientWidth).toFixed(5);
+                        var y = (thisPos.top/document.getElementById("cnvs").clientHeight).toFixed(5);
 
                         finalposition.width = x;
                         finalposition.height = y;
                     }
 
-
-
                     $(function () {
                         
-
                         image1url = "img/demo/amulet96.png";
                         image2url = "img/demo/Bugatti-Chiron-2016.jpg";                    
                         
                         makeimage(image1url, image2url);
-
-                        
-
 
                         var windowWidth = $(window).width();
                         $(window).resize(function () {
@@ -163,6 +116,7 @@
                         });
                         
                         $(window).bind("load", function() {
+                            RelativeImgDimensions();
                             RelativeIconDimensions();
                             startingPosition();
                          });
